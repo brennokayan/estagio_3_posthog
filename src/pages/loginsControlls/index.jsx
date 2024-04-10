f8a7c9c22a0f80b5f84ecd11c18d86a86f38dbf3
brennokayan <brennokayan@hotmail.com>
2024-04-10 16:44:10 -0400

    corrigindo a navegação entre telas

--- src/pages/loginsControlls/index.jsx
+++ src/pages/loginsControlls/index.jsx
@@ -1,35 +1,55 @@
-import {Box} from "@mui/material"
-export function LoginsControlls(){
-    const logins = [
-        {
-            name: "Login1 Test",
-            path: "/login1/test"
-        },
-        {
-            name: "Login1 Control",
-            path: "/login1/control"
-        },
-        {
-            name: "Login2 Test",
-            path: "/login2/test"
-        },
-        {
-            name: "Login2 Control",
-            path: "/login2/control"
-        },
-     ]
-    return(
-        <>
-            <Box>
-                <h1>Logins Controlls</h1>
-                {
-                    logins.map((login, index) => (
-                        <Box key={index}>
-                            <a href={login.path}>{login.name}</a>
-                        </Box>
-                    ))
-                }
-            </Box>
-        </>
-    )
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+export function LoginsControlls() {
+  const logins = [
+    {
+      name: "Login1 Test",
+      path: "/login1/test",
+    },
+    {
+      name: "Login1 Control",
+      path: "/login1/control",
+    },
+    {
+      name: "Login2 Test",
+      path: "/login2/test",
+    },
+    {
+      name: "Login2 Control",
+      path: "/login2/control",
+    },
+  ];
+  return (
+    <>
+      <Box
+        height={"100vh"}
+        width={"100vw"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+      >
+        <h1>Logins Controlls</h1>
+        {logins.map((login, index) => (
+          <Box key={index} display={"flex"}>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "black",
+                padding: "10px",
+                border: "1px solid black",
+                borderRadius: "5px",
+                width: "150px",
+                textAlign: "center",
+                margin: "4px",
+              }}
+              to={login.path}
+            >
+              {login.name}
+            </Link>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
 }
