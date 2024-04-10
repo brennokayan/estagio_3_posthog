a49bc835678087a474b62a7030f0ae113c826495
brennokayan <brennokayan@hotmail.com>
2024-04-10 17:12:42 -0400

    fix: colocando cada pagina de login no perfil de teste

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import { Login1Control } from "./pages/login1-control";
-import { Login1Test } from "./pages/login1-test";
+// import { Login1Test } from "./pages/login1-test";
 import { Login2Test } from "./pages/login2-teste";
-import { Login2Control } from "./pages/login2-control";
+// import { Login2Control } from "./pages/login2-control";
 import { LoginsControlls } from "./pages/loginsControlls";
 
 
@@ -12,10 +12,10 @@ export function App(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LoginsControlls/>}/>
-                <Route path="/login1/test" element={<Login1Test/>}/>
+                {/* <Route path="/login1/test" element={<Login1Test/>}/> */}
                 <Route path="/login1/control" element={<Login1Control/>}/>
                 <Route path="/login2/test" element={<Login2Test/>}/>
-                <Route path="/login2/control" element={<Login2Control/>}/>
+                {/* <Route path="/login2/control" element={<Login2Control/>}/> */}
             </Routes>
         </BrowserRouter>
     )
--- src/pages/loginsControlls/index.jsx
+++ src/pages/loginsControlls/index.jsx
@@ -3,20 +3,12 @@ import { Link } from "react-router-dom";
 export function LoginsControlls() {
   const logins = [
     {
-      name: "Login1 Test",
-      path: "/login1/test",
-    },
-    {
       name: "Login1 Control",
       path: "/login1/control",
     },
     {
       name: "Login2 Test",
       path: "/login2/test",
-    },
-    {
-      name: "Login2 Control",
-      path: "/login2/control",
     },
   ];
   return (
